30a1f83a1ef953938447535d17a7367f4e28a3f6
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-06-13 07:50:27 -0400

    Create build.sh

---
+++ babeld/build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+case "$ARCH" in
+  amd64)
+    PKG_ARCH="amd64"
+  ;;
+  i386)
+    PKG_ARCH="386"
+    exit 0
+  ;;
+  armhf)
+    exit 0
+    PKG_ARCH="arm";
+  ;;
+  arm64)
+    exit 0
+    PKG_ARCH="arm64";
+  ;;
+  *)
+    echo "Unknown Arch"
+    exit 1
+  ;;
+esac
+
+# Prep working directory
+mkdir root
+mkdir tmp
+
+# Prepare root directory
+cp -R files/* root/
+
+git clone git://github.com/jech/babeld.git /tmp/babeld/tmp
+cd tmp
+sed -i 's|PREFIX = /usr/local|PREFIX = $(pwd)/..root/ |' Makefile
+make
+make install
+cd ..
+rm -rf tmp
+
+# Make deb pacakges
+version="$(root/bin/babeld -V  2>&1)"
+version=${version:7}
+echo "Version: $version" >> root/DEBIAN/control
+#echo "Architecture: $( dpkg --print-architecture)" >> root/DEBIAN/control
+echo Architecture: $ARCH >> root/DEBIAN/control
+
+dpkg-deb --build root
+rm -rf root
+mv root.deb ../BABELD-$version-$ARCH.deb
+
+# Install and cleanup
+cd ..
+rm -rf tmp
