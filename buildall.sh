03dabadd74966d6e4520e6bfe558fe18f0d8d5a3
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-06-13 07:56:51 -0400

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+apt-get install -y gcc-arm-linux-gnueabihf
+
 ARCHS="armhf arm64 i386 amd64"
 PKGS="ipfs ipfs-tomesh babeld babeld-tomesh"
+
+PKGS="babeld"
+ARCHS="armhf"
+
 for PKG in $PKGS; do
 
     for ARCH in $ARCHS; do
