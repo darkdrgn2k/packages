3a1603ae7c30ad681d19a07d8e081cd4d4d71696
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-06-22 01:04:52 -0400

    Babeld tomesh (#4)
    Added babled-tomesh

---
+++ confset/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+case "$ARCH" in
+  noarch)
+    PKG_ARCH="noarch"
+  ;;
+  *)
+    exit 0
+  ;;
+esac
+
+# Prep working directory
+mkdir root
+mkdir tmp
+root=$(pwd)
+
+# Prepare root directory
+cp -R files/* root/
+chmod 755 root/DEBIAN/postinst
+
+version=1
+
+# Make deb pacakges
+echo "Version: $version" >> root/DEBIAN/control
+echo Architecture: $ARCH >> root/DEBIAN/control
+
+sudo chown -R root.root root
+dpkg-deb --build root
+sudo rm -rf root
+mv root.deb ../confget-$version-$ARCH.deb
+
+# Install and cleanup
+rm -rf tmp
