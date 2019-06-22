7f7e7e0e1dff023616f80c05adfb71bb4f76db65
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-06-22 00:42:03 -0400

    Update build.sh

--- babeld/build.sh
+++ babeld/build.sh
@@ -57,6 +57,27 @@ dpkg-deb --build root
 sudo rm -rf root
 mv root.deb ../babeld-$version-$ARCH.deb
 
+# Install and cleanup
+rm -rf tmp
+
+# Babeld-tomesh
+# Prep working directory
+mkdir root
+mkdir tmp
+root=$(pwd)
+
+# Prepare root directory
+cp -R files-tomesh/* root/
+chmod 755 root/DEBIAN/postinst
+
+# Make deb pacakges
+echo "Version: $version" >> root/DEBIAN/control
+echo Architecture: $ARCH >> root/DEBIAN/control
+
+sudo chown -R root.root root
+dpkg-deb --build root
+sudo rm -rf root
+mv root.deb ../babeld-tomesh-$version-$ARCH.deb
 
 # Install and cleanup
 rm -rf tmp
