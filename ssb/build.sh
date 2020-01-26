542f8b59ff5d3c2baf520f0e16a8ce8c08f0cbe0
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2020-01-26 17:29:38 -0500

    Create build.sh

---
+++ ssb/build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+case "$ARCH" in
+  amd64)
+    PKG_ARCH="amd64"
+  ;;
+  i386)
+   exit 0
+  ;;
+  armhf)
+   exit 0
+  ;;
+  arm64)
+    exit 0
+  ;;
+  *)
+    exit 0
+  ;;
+esac
+## Move into main build
+NODEJS_PREFIX=10
+NODEJS_VERSION="$NODEJS_PREFIX.15.3"
+curl -sL https://deb.nodesource.com/setup_$NODEJS_PREFIX.x | sudo -E bash -
+apt-get install -y nodejs
+
+# Prep working directory
+mkdir root
+mkdir tmp
+root=$(pwd)
+
+# Prepare root directory
+cp -R files/* root/
+chmod 755 root/DEBIAN/postinst
+
+
+
+sudo apt-get install -y socat python-dev libtool python-setuptools autoconf automake
+sudo sudo npm install ssb-server
+rm -rf tmp
+
+sudo chown -R root.root root
+
+dpkg-deb --build root
+sudo rm -rf root
+mv root.deb ../ssb-$version-$ARCH.deb
+
+# Install and cleanup
+rm -rf tmp
