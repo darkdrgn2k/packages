83eba5d72f944801e667b44376c4c3d81c128117
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2020-01-30 18:56:05 -0500

    SSB (#7)
    Added SSB x64

--- buildall.sh
+++ buildall.sh
@@ -2,22 +2,31 @@
 # crossbuild-essential-armhf libc6-dev:armhf g++-arm-linux-gnueabihf gcc-arm-linux-gnueabihf
 
 sudo apt-get update
-sudo apt-get install -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
-sudo apt-get install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
 
+sudo apt-get install -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf # ARM  Cross COmpiler
+sudo apt-get install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu  # ARM64 Cross Compiler
+
+# Go Dev Environment
 wget https://dl.google.com/go/go1.11.linux-arm64.tar.gz 
 sudo tar -C /usr/local -xzf go1.11.linux-arm64.tar.gz
 rm -rf go1.11.linux-arm64.tar.gz
 
+# NODE JS
+NODEJS_PREFIX=10
+NODEJS_VERSION="$NODEJS_PREFIX.15.3"
+curl -sL https://deb.nodesource.com/setup_$NODEJS_PREFIX.x | sudo -E bash -
+sleep 1
+sudo apt-get install -y nodejs
+
+# I think some stuff for SSB
+sudo apt-get install -y python-dev libtool python-setuptools autoconf automake
+
 GOROOT=/usr/local/go
 
-
 ARCHS="i386 amd64 armhf arm64 all"
-#PKGS="ipfs ipfs-tomesh"
-PKGS="babeld babeld-tomesh confset yggdrasil-iptunnel aether"
+PKGS="babeld babeld-tomesh confset yggdrasil-iptunnel ssb aether"
 
 for PKG in $PKGS; do
-
     for ARCH in $ARCHS; do
         export ARCH
         cd $PKG
---
+++ ssb/build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+case "$ARCH" in
+  amd64)
+    PKG_ARCH="amd64"
+  ;;
+  i386)
+   exit 0
+  ;;
+  armhf)
+     export CC="CC=arm-linux-gnueabihf-gcc"
+     export CXX="CXX=arm-linux-gnueabihf-g++"
+     exit 0
+  ;;
+ arm64)
+     export CC="aarch64-linux-gnu-gcc"
+     export CXX="aarch64-linux-gnu-g++"
+     exit 0
+  ;;
+  *)
+    exit 0
+  ;;
+esac
+
+version=0.0.1
+
+# Prep working directory
+mkdir root
+root=$(pwd)
+
+# Prepare root directory
+cp -R files/* root/
+chmod 755 root/DEBIAN/postinst
+
+sudo apt-get install -y socat python-dev libtool python-setuptools autoconf automake
+
+cd root
+#sudo sudo sudo npm install --target_arch=$ARCH --target_platform=linux --prefix `pwd` --global --unsafe-perms sodium-native@2.4.2
+npm install --target_arch=$ARCH --target_platform=linux --prefix `pwd` --global --unsafe-perms ssb-server
+
+cd ..
+
+echo "Version: $version" >> root/DEBIAN/control
+echo Architecture: $ARCH >> root/DEBIAN/control
+sudo chown -R root.root root
+
+dpkg-deb --build root
+
+sudo rm -rf root
+mv root.deb ../ssb-$version-$ARCH.deb
+
+# Install and cleanup
+rm -rf tmp
