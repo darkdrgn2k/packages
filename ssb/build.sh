12559d6971a101709c6d9b32ecfa54796c801d42
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2020-04-10 17:18:06 -0400

    Update build.sh

--- ssb/build.sh
+++ ssb/build.sh
@@ -32,8 +32,8 @@ chmod 755 root/DEBIAN/postinst
 sudo apt-get install -y socat python-dev libtool python-setuptools autoconf automake
 
 cd root
-#sudo sudo sudo npm install --target_arch=$ARCH --target_platform=linux --prefix `pwd` --global --unsafe-perms sodium-native@2.4.2
-npm install --target_arch=$ARCH --target_platform=linux --prefix `pwd` --global --unsafe-perms ssb-server
+#sudo sudo sudo npm install --target_arch=$ARCH --target_platform=linux --prefix `pwd` --global --unsafe-perm=true sodium-native@2.4.2
+npm install --target_arch=$ARCH --target_platform=linux --prefix `pwd` --global --unsafe-perm=true ssb-server
 
 cd ..
 
