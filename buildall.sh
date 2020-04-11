63dc0881775571f520b6f881c1ad5e861bbbbb44
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2020-04-10 22:42:02 -0400

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -32,7 +32,7 @@ sudo mkdir 1
 sudo mount -o loop,offset=$((512*$SECTORSTART)) 2020-02-13-raspbian-buster-lite.img 1
 sudo cp /usr/bin/qemu-arm-static 1/usr/bin  #Allow chroot
 sudo mkdir /opt/rpifs
-sudo cp -r 1/ /opt/rpifs/
+sudo cp -r 1/. /opt/rpifs
 sudo ls -la /opt/rpifs
 sudo umount 1
 
